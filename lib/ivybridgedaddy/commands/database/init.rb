ede3c5c2535e004eafb1019ee08dd9431658bd76
Kentaro Hayashi <kenhys@gmail.com>
2019-02-23 23:56:50 +0900

    Define initial schema

--- lib/ivybridgedaddy/commands/database/init.rb
+++ lib/ivybridgedaddy/commands/database/init.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../../command'
+require_relative '../../config'
+require_relative '../../dbutil'
 
 module IvyBridgeDaddy
   module Commands
@@ -9,11 +11,72 @@ module IvyBridgeDaddy
         def initialize(path, options)
           @path = path
           @options = options
+          @config = ::IvyBridgeDaddy::Config.new
+          IvyBridgeDaddy::DatabaseUtility.create_or_open_database(@config.database_path)
         end
 
         def execute(input: $stdin, output: $stdout)
-          # Command logic goes here ...
-          output.puts "OK"
+          Groonga::Schema.define do |schema|
+            schema.create_table("Makers", options = {:type => :patricia_trie}) do |table|
+              table.text("name")
+              table.text("url")
+            end
+
+            schema.create_table("Cpus", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Memories", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Storages", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Graphics", options = {:type => :patricia_trie}) do |table|
+            end
+
+            schema.create_table("Boards", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Drives", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("OperatingSystems", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Cases", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("FormFactors", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Powers", options = {:type => :patricia_trie}) do |table|
+            end
+
+            schema.create_table("Models", options = {:type => :patricia_trie}) do |table|
+              table.text("code")
+              table.text("catch_phrase")
+              table.text("name")
+              table.text("detail")
+              table.reference("cpu", "Cpus")
+              table.reference("memory", "Memories")
+              table.reference("storage", "Storages")
+              table.reference("graphic", "Graphics")
+              table.reference("board", "Boards")
+              table.reference("drive", "Drives")
+              table.reference("os", "OperatingSystems")
+              table.reference("case", "Cases")
+              table.reference("formfactor", "FormFactors")
+              table.reference("power", "Powers")
+              table.time("created_at")
+              table.time("updated_at")
+            end
+            schema.create_table("Options", options = {:type => :patricia_trie}) do |table|
+            end
+            schema.create_table("Prices", options = {:type => :patricia_trie}) do |table|
+            end
+
+            schema.create_table("Specs", options = {:type => :patricia_trie}) do |table|
+              table.reference("model", "Models")
+              table.reference("cpu", "Cpus")
+              table.reference("memory", "Memories")
+              table.time("created_at")
+              table.time("updated_at")
+            end
+
+          end
+
         end
       end
     end
