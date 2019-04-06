72ccfbba9203f808979ef18a70eadbbcddfeeff1
Kentaro Hayashi <kenhys@gmail.com>
2019-04-06 13:52:14 +0900

    Add helper to return fixture path

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,1 +1,8 @@
 
+def fixture_path(*elements)
+  File.join(File.dirname(__FILE__), "fixtures", *elements)
+end
+
+def fixture_file_path(*elements)
+  File.join("file://" + File.dirname(__FILE__), "fixtures", *elements)
+end
