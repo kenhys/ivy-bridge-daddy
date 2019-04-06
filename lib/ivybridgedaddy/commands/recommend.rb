f933a848c7abea96afc756c796ae8e70576a1087
Kentaro Hayashi <kenhys@gmail.com>
2019-04-06 17:00:00 +0900

    Add initial recommend

--- lib/ivybridgedaddy/commands/recommend.rb
+++ lib/ivybridgedaddy/commands/recommend.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require_relative '../command'
+require_relative '../config'
+require_relative '../dbutil'
 
 module IvyBridgeDaddy
   module Commands
@@ -8,11 +10,29 @@ module IvyBridgeDaddy
       def initialize(option, options)
         @option = option
         @options = options
+        @config = ::IvyBridgeDaddy::Config.new
+        IvyBridgeDaddy::DatabaseUtility.create_or_open_database(@config.database_path)
+        @specs = Groonga["Specs"]
+        @context = Groonga::Context.default
       end
 
       def execute(input: $stdin, output: $stdout)
         # Command logic goes here ...
-        output.puts "OK"
+        records = @context.select(@specs,
+                                  :filter => "memory.module_total >= 16 ",
+                                  :output_columns => "_key,_score,model,price,memory.module_total,model.storage",
+                                  :sort_keys => "_score,price",
+                                  :limit => -1)
+        records.each do |record|
+          entry = "%d %s %s %s %s" % [
+                 record["_score"],
+                 record.price,
+                 record.model,
+                 record["memory.module_total"],
+                 record["model.storage"]
+          ]
+          puts entry
+        end
       end
     end
   end
